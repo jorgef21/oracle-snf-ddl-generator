4d06ee9a250bb9b4832d5d3302ebc481c19c607d
George-Flores <george.flores@laureate.net>
2020-04-28 16:03:50 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # oracle-snf-ddl-generator
-DDL generator to parse Oracle to Snowflake tables.
+DDL Generator using metadata
+
+# Requirements:
+- CSV file as input with headers:
+TABLE_NAME	COLUMN_NAME	DATA_TYPE	DATA_LENGTH
+EMPLOYEE    ID          NUMBER      22
+EMPLOYEE    NAME        VARCHAR     255
+EMPLOYEE    LAST_NAME   VARCHAR     255
+EMPLOYEE    DOB         DATE        7
+
+- .sql Template for the script 
+INSERT.sql = INSERT INTO {{ TABLE_NAME }} VALUES {1,2,3}
+OUTPUT FOLDER
+To output the scripts using the template.
--- TEMPLATES/DDL_METADATA.sql
+++ TEMPLATES/DDL_METADATA.sql
@@ -4,8 +4,5 @@ CREATE TABLE {{ data[0].TABLE_NAME }}(
     {% elif col.DATA_TYPE == "VARCHAR2" %}VARCHAR({{ col.DATA_LENGTH}})
     {% elif col.DATA_TYPE == "VARCHAR" %}VARCHAR({{ col.DATA_LENGTH}})
     {% elif col.DATA_TYPE == "TIMESTAMP(6)" %}TIMESTAMP_NTZ(9)
-    {% elif col.DATA_TYPE == "CHAR" %}VARCHAR
-    {% elif col.DATA_TYPE == "RAW" %}BINARY
-    {% elif col.DATA_TYPE == "ROWID" %}VARCHAR
-    {% endif %}{% if not loop.last %},{% endif %}{% endfor %}
+    {% elif col.DATA_TYPE == "CHAR" %}VARCHAR{% elif col.DATA_TYPE == "RAW" %}BINARY{% elif col.DATA_TYPE == "ROWID" %}VARCHAR{% endif %}{% if not loop.last %},{% endif %}{% endfor %}
 );  
